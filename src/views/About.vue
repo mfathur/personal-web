3f14da59ca01ae9c7c0258da870e96b59c856154
M Fathur <mfathur17@gmail.com>
2022-02-13 20:15:24 +0700

    init tailwind css

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="font-bold bg-haiti text-white text-4xl">This is an about pages</h1>
+    <h6 class="bg-red-500">Hello worlds</h6>
   </div>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ['Quicksand', 'Roboto'],
+    },
+    extend: {
+      colors: {
+        haiti: '#0E1630',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};

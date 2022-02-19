eba3b3ec7b18d107559eab375ea29eccf851e28d
M Fathur <mfathur17@gmail.com>
2022-02-19 15:49:50 +0700

    fix font size in navbar

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -7,7 +7,7 @@
                 <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </button>
         <div class="block w-full md:w-auto" id="mobile-menu">
-        <ul class="flex flex-col mt-4 font-medium text-white md:flex-row md:space-x-8 md:mt-3 md:text-sm md:font-bold">
+        <ul class="flex flex-col mt-4 font-medium text-white md:flex-row md:space-x-8 md:mt-3 md:text-base md:font-bold">
                 <li>
                     <router-link to="/" class="block py-2 pl-3 pr-4 rounded md:bg-transparent" aria-current="page">Home</router-link>
                 </li>

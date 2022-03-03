c8277b07aee2dc907d13ed500ba37a55041e4d2c
M Fathur <mfathur17@gmail.com>
2022-03-03 11:09:54 +0700

    add overflow scroll y in contact me message

--- src/components/ContactMe.vue
+++ src/components/ContactMe.vue
@@ -22,7 +22,7 @@
           <form>
                 <input type="text" class="w-full px-4 py-2 mb-4 font-bold text-white rounded-lg bg-haiti-700" placeholder="Your name"><br>
                 <input type="text" class="w-full px-4 py-2 mb-4 font-bold text-white rounded-lg bg-haiti-700" placeholder="Your email address"><br>
-                <textarea type="text" class="w-full px-4 py-2 mb-2 font-bold text-white rounded-lg resize-none bg-haiti-700" cols="50" rows="8" placeholder="Message"/><br>
+                <textarea type="text" class="w-full px-4 py-2 mb-2 overflow-y-scroll font-bold text-white rounded-lg resize-none bg-haiti-700" cols="50" rows="8" placeholder="Message"/><br>
                 <button class="px-5 py-2 font-bold rounded-lg bg-haiti-700 hover:bg-haiti-500">Submit</button>
             </form>
       </div>
